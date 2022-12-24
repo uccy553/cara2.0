2dd4463a96e671a492a32bd3a9916bcd2779b0bf
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-24 20:07:18 +0100

    Update Newsletter.jsx

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -9,7 +9,7 @@ function Newsletter() {
         <h1 className='text-[1.8em] text-white font-semibold'>Sign up for our newsletter</h1>
         <div>
             <input type="email" placeholder="Enter your email" className="border px-2 py-1 rounded-md focus:outline-none focus:shadow-outline-blue" />
-        <button className="mt-3 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full focus:outline-none focus:shadow-outline-blue">
+        <button className="mt-2 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full focus:outline-none focus:shadow-outline-blue">
             Subscribe
         </button>
         </div>
